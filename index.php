b546ec8a420412a4c5bed273ef590406b8d1b39a
chalanadulaj <chalanadulaj99@gmail.com>
2018-06-25 14:02:58 +0530

    #1 Add Template

---
+++ index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Lotus Cabs - Taxi Company HTML5 Template</title>
+
+    <!-- Standard Favicon -->
+    <link rel="icon" type="image/x-icon" href="images/lotus-logo.png" />
+    <!-- For iPhone 4 Retina display: -->
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114x114-precomposed.png">
+    <!-- For iPad: -->
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72x72-precomposed.png">
+    <!-- For iPhone: -->
+    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-57x57-precomposed.png">	
+    <!-- Library - Loader CSS -->
+    <link rel="stylesheet" type="text/css" href="libraries/loader/loaders.min.css">
+
+    <!-- Library - Google Font Familys -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,100italic,100,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Library - Bootstrap v3.3.5 -->
+    <link rel="stylesheet" type="text/css" href="libraries/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="libraries/bootstrap/bootstrap-datetimepicker.min.css">
+    <!-- Font Icons -->
+    <link rel="stylesheet" type="text/css" href="libraries/fonts/font-awesome.min.css">
+    <!-- Library - OWL Carousel V.2.0 beta -->
+    <link rel="stylesheet" type="text/css" href="libraries/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="libraries/owl-carousel/owl.theme.css">
+    <!-- Library - FlexSlider v2.5.0 -->
+    <link rel="stylesheet" type="text/css" href="libraries/flexslider/flexslider.css">
+    <!-- Library - Animate CSS -->
+    <link rel="stylesheet" type="text/css" href="libraries/animate/animate.min.css">
+    <!-- Custom - Common CSS -->
+    <link rel="stylesheet" type="text/css" href="css/plugins.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation-menu.css">
+    <!-- Custom - Theme CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/shortcodes.css">
+
+</head>
+
+<body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
+    <!-- Loader -->
+    <div id="site-loader" class="load-complete">
+        <div class="loader">
+            <div class="loader-inner ball-clip-rotate">
+                <div></div>
+            </div>
+        </div>
+    </div><!-- Loader /- -->
+
+    <a id="top"></a>
+    <!-- Main Container -->
+    <div class="main-container">
+
+        <!-- Header -->
+        <?php include './header.php'; ?>
+        <!-- Header /- -->
+
+        <!-- Photo Slider -->	
+        <?php include './slider.php'; ?>
+        <!-- Photo Slider/- -->
+        <!-- What We Do -->
+        <div class="container-fluid no-padding what-we-do">
+            <div class="section-padding"></div>
+            <div class="image-box" style="padding-top: 40px;">
+                <img src="photos/10.jpg" alt=""/>
+            </div>
+            <!-- Container -->
+            <div class="container" >			
+                <div class="col-md-8 pull-right">
+                    <div class="we-do-more ">
+                        <h3>Welcome to </h3>
+                        <h2>lotus cabs - Sri Lanka</h2>
+                        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                    </div>
+
+                </div>
+            </div><!-- Container/-  -->
+            <div class="section-padding"></div>
+        </div>
+        <!-- What We Do/- -->
+        <!-- Choose Us -->
+        <div class="container-fluid no-padding " style="padding-top: 140px;">
+            <div class="section-padding"></div>
+            <!-- Container -->
+            <div class="container service-b">
+                <div class="section-header2 we-do-more2">
+                    <h2>Services </h2>				
+                </div>
+                <!-- Choose Carousel -->
+                <div class="choose-carousel">			
+                    <div class="col-md-12">
+                        <div class="choose-us-box">
+                            <div class="choose-img-box1">
+                                <img src="photos/4.jpg" alt=""/>
+                            </div>
+                            <div class="choose-us-content-box">
+                                <h3>taxi</h3>
+                                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, mollit anim id est laborum cillum dolore eu fugiat nulla pariatur.</p>
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="choose-us-box">
+                            <div class="choose-img-box1">
+                                <img src="photos/3.jpg" alt=""/>
+                            </div>
+                            <div class="choose-us-content-box">
+                                <h3>Airport drop</h3>
+                                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, mollit anim id est laborum cillum dolore eu fugiat nulla pariatur.</p>
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="choose-us-box">
+                            <div class="choose-img-box1">
+                                <img src="photos/5.jpg" alt=""/>
+                            </div>
+                            <div class="choose-us-content-box">
+                                <h3>Tours</h3>
+                                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit,  mollit anim id est laborum cillum dolore eu fugiat nulla pariatur.</p>
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- Choose Carousel/- -->
+            </div><!-- Container/- -->
+        </div>
+        <!-- Choose Us/- -->
+
+
+
+
+        <!-- -news -->
+        <div class="container-fluid no-padding blog">
+            <div class="section-padding1"></div>
+            <div class="container">			
+                <div class="section-header we-do-more2">
+                    <h2>News</h2>				
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <article class="blog-post-list">
+                            <div class="entry-cover">
+                                <a href="#">
+                                    <img src="photos/s1.jpg" alt=""/>
+                                </a>
+                                <div class="post-meta">
+                                    <img src="images/icon/photo.png" alt="photo"/>
+                                </div>
+                                <div class="entry-meta">
+                                    <div class="meta-inner">
+                                        <div class="by-line pull-left">Posted By <a href="#">Admin</a></div>
+                                        <div class="post-comment pull-right">
+                                            <a href="#"><i class="fa fa-comment"></i>Comments<span>14</span></a>
+                                        </div>
+                                    </div>								
+                                    <div class="meta-inner">
+                                        <div class="post-date pull-left"><span>On 22 Aug 2015</span></div>
+                                        <div class="tags pull-right">
+                                            <i class="fa fa-tag"></i>
+                                            <a href="#">Cab</a>
+                                            <a href="#">Taxi</a>
+                                            <a href="#">Rent</a>
+                                            <a href="#">Hire</a>
+                                            <a href="#">Trip</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="blog-content">
+                                <h3 class="entry-title">Well Trained Drivers On Duty</h3>
+                                <div class="entry-content">
+                                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <a href="#" title="Read More" class="pull-right">Read More</a>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <article class="blog-post-list">
+                            <div class="entry-cover">
+                                <a href="#">
+                                    <img src="photos/s2.jpg" alt=""/>
+                                </a>
+                                <div class="post-meta">
+                                    <img src="images/icon/video.png" alt="video"/>
+                                </div>
+                                <div class="entry-meta">
+                                    <div class="meta-inner">
+                                        <div class="by-line pull-left">Posted By <a href="#">Admin</a></div>
+                                        <div class="post-comment pull-right">
+                                            <a href="#"><i class="fa fa-comment"></i>Comments<span>22</span></a>
+                                        </div>
+                                    </div>
+                                    <div class="meta-inner">
+                                        <div class="post-date pull-left"><span>On 14 Sep 2015</span></div>
+                                        <div class="tags pull-right">
+                                            <i class="fa fa-tag"></i>
+                                            <a href="#">Pickup</a>
+                                            <a href="#">Airport</a>
+                                            <a href="#">paid</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="blog-content">
+                                <h3 class="entry-title">Got The Airport Shuttle Contract</h3>
+                                <div class="entry-content">
+                                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <a href="#" class="pull-right">Read More</a>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <article class="blog-post-list">
+                            <div class="entry-cover">
+                                <a href="#">
+                                    <img src="photos/s3.jpg" alt=""/>
+                                </a>
+                                <div class="post-meta">
+                                    <img src="images/icon/photo.png" alt="photo"/>
+                                </div>
+                                <div class="entry-meta">
+                                    <div class="meta-inner">
+                                        <div class="by-line pull-left">Posted By <a href="#">Admin</a></div>
+                                        <div class="post-comment pull-right">
+                                            <a href="#"><i class="fa fa-comment"></i>Comments<span>16</span></a>
+                                        </div>
+                                    </div>								
+                                    <div class="meta-inner">
+                                        <div class="post-date pull-left"><span>On 30 Oct 2015</span></div>
+                                        <div class="tags pull-right">
+                                            <i class="fa fa-tag"></i>
+                                            <a href="#">City</a>
+                                            <a href="#">Service</a>
+                                            <a href="#">Taxi</a>
+                                            <a href="#">Cab</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="blog-content">
+                                <h3 class="entry-title">Why Should Companies Offer Taxi</h3>
+                                <div class="entry-content">
+                                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <a href="#" class="pull-right">Read More</a>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div class="section-padding"></div>
+        </div><!-- - news-  -->
+        <!-- Testimonial -->
+        <div class="container-fluid no-padding testimonial">
+            <img src="images/border-pattern.jpg" alt="border"/>
+            <div class="section-padding1"></div>
+            <div class="container">
+                <div class="section-header we-do-more2">
+                    <h2>Words from Our Customers</h2>				
+                </div>
+                <div id="testimonial-slider" class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <p><i class="fa fa-quote-left"></i>Now the world do not move to the beat of just one drum what might be right for you may not be right for some makin their way the only way they know how that's just a little bit more than the law will allow wouldn't you like<i class="fa fa-quote-right"></i></p>
+                            <span style="padding-bottom: 15px;">Thomas Knoll</span>
+                            <div class="text-center">
+                                <img src="images/services/Kathrin.png" alt="" width="10%" />
+                            </div>
+
+                        </div>
+
+                        <div class="item">
+                            <p><i class="fa fa-quote-left"></i>Now the world do not move to the beat of just one drum what might be right for you may not be right for some makin their way the only way they know how that's just a little bit more than the law will allow wouldn't you like<i class="fa fa-quote-right"></i></p>
+                            <span style="padding-bottom: 15px;">Thomas Knoll</span>
+                            <div class="text-center">
+                                <img src="images/services/Kathrin.png" alt="" width="10%" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-padding"></div>	
+            <img src="images/border-pattern.jpg" alt="border"/>
+        </div><!-- Testimonial/- -->
+
+        <!--Footer -->
+        <?php include './footer.php'; ?>
+        <!--Footer End-->
+
+
+    </div><!-- Main Container -->
+    <!-- JQuery v1.11.3 -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Library - Modernizer -->
+    <script src="libraries/modernizr/modernizr.js"></script>
+
+    <!-- Library - Bootstrap v3.3.5 -->
+    <script src="libraries/bootstrap/bootstrap.min.js"></script><!-- Bootstrap JS File v3.3.5 -->
+    <script src="libraries/bootstrap/bootstrap-datetimepicker.min.js"></script><!-- Bootstrap JS File v3.3.5 -->
+
+    <!-- jQuery Easing v1.3 -->
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- Library - jQuery.appear -->
+    <script src="libraries/appear/jquery.appear.js"></script>
+
+    <!-- Library - OWL Carousel V.2.0 beta -->
+    <script src="libraries/owl-carousel/owl.carousel.min.js"></script>
+
+    <!-- jQuery For Number Counter -->	
+    <script src="libraries/number/jquery.animateNumber.min.js"></script>
+
+    <!-- Library - Google Map API -->
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+
+    <!-- Library - FlexSlider v2.5.0 -->
+    <script defer src="libraries/flexslider/jquery.flexslider.js"></script>
+
+    <!-- Library - Theme JS -->
+    <script src="js/functions.js"></script>
+</body>
+</html>
