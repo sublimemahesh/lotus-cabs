8b0bacf13a1dc585ccbece03fe79683d4d832785
chalanadulaj <chalanadulaj99@gmail.com>
2018-06-28 10:30:50 +0530

    #5 tour package design -day wise

---
+++ tour-packages-view.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Lotus Cabs - Taxi Company HTML5 Template</title>
+
+    <!-- Standard Favicon -->
+    <link rel="icon" type="image/x-icon" href="images/lotus-logo.png" />
+    <!-- For iPhone 4 Retina display: -->
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114x114-precomposed.png">
+    <!-- For iPad: -->
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72x72-precomposed.png">
+    <!-- For iPhone: -->
+    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-57x57-precomposed.png">	
+    <!-- Library - Loader CSS -->
+    <link rel="stylesheet" type="text/css" href="libraries/loader/loaders.min.css">
+
+    <!-- Library - Google Font Familys -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,100italic,100,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Library - Bootstrap v3.3.5 -->
+    <link rel="stylesheet" type="text/css" href="libraries/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="libraries/bootstrap/bootstrap-datetimepicker.min.css">
+    <!-- Font Icons -->
+    <link rel="stylesheet" type="text/css" href="libraries/fonts/font-awesome.min.css">
+    <!-- Library - OWL Carousel V.2.0 beta -->
+    <link rel="stylesheet" type="text/css" href="libraries/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="libraries/owl-carousel/owl.theme.css">
+    <!-- Library - FlexSlider v2.5.0 -->
+    <link rel="stylesheet" type="text/css" href="libraries/flexslider/flexslider.css">
+    <!-- Library - Animate CSS -->
+    <link rel="stylesheet" type="text/css" href="libraries/animate/animate.min.css">
+    <!-- Custom - Common CSS -->
+    <link rel="stylesheet" type="text/css" href="css/plugins.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation-menu.css">
+    <!-- Custom - Theme CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/shortcodes.css">
+    <link href="css/lightbox.css" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
+    <!-- Loader -->
+    <div id="site-loader" class="load-complete">
+        <div class="loader">
+            <div class="loader-inner ball-clip-rotate">
+                <div></div>
+            </div>
+        </div>
+    </div><!-- Loader /- -->
+
+    <a id="top"></a>
+    <!-- Main Container -->
+    <div class="main-container">
+
+        <!-- Header -->
+        <?php include './header.php'; ?>
+        <!-- Header /- -->
+
+        <div class="container-fluid no-padding page-content">
+            <!-- Page Content -->
+            <div class="section-padding"></div>
+            <!-- Container -->
+            <div class="container">		
+                <!-- Blog Area -->
+                <div class="col-md-12 blog-area" style="padding:20px 0px ">		
+                    <div class="section-header">
+                        <h3>Recent Updates From Our Blog</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+
+                        <a class="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1" data-title="option caption"><img class="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="image-1" /></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a class="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1"><img class="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="image-1" /></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a class="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1"><img class="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="image-1" /></a>
+
+                    </div>
+                    <div class="col-md-3">
+                        <a class="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1"><img class="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="image-1" /></a>
+                    </div>
+
+
+
+                </div>
+               
+                <div class="row" >
+                    <div class="col-md-12"style="padding-top: 20px;">
+                        <div class="blog-content">
+                            <h3 class="entry-title">>.Day 1- Well Trained Drivers On Duty</h3>
+                            <div class="entry-content">
+                                <p>Come and listen to a story about a man named Jed a poor mountaineer barely kept his family fed so lets make the most of this beautiful day since we're together It is a beautiful day in this neighborhood a beautiful day for a neighbor would you be mine. Could you be mine Its a neighborly day in this beautywood a neighborly day for a beauty. Would you be mine. Its a neighborly day in this beautywood a neighborly day for a beauty.</p>
+                                <p>The Brady Bunch the Brady Bunch that's the way we all became the Brady Bunch. Now the world don't move to the beat of just one drum. What might be right for you may not be right for some. Movin' on up to the east side. We finally got a piece of the pie. If you have a problem if no one else can help and if you can find them maybe you can hire The A-Team.</p>
+
+                                <p>Could you be mine Its a neighborly day in this beautywood a neighborly day for a beauty. Would you be mine. Its a neighborly day in this beautywood a neighborly day for a beauty the Brady Bunch the Brady Bunch.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- Container/- -->
+            <div class="section-padding"></div>				
+        </div>
+
+
+        <!--Footer -->
+        <?php include './footer.php'; ?>
+        <!--Footer End-->
+
+
+    </div><!-- Main Container -->
+    <!-- JQuery v1.11.3 -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Library - Modernizer -->
+    <script src="libraries/modernizr/modernizr.js"></script>
+
+    <!-- Library - Bootstrap v3.3.5 -->
+    <script src="libraries/bootstrap/bootstrap.min.js"></script><!-- Bootstrap JS File v3.3.5 -->
+    <script src="libraries/bootstrap/bootstrap-datetimepicker.min.js"></script><!-- Bootstrap JS File v3.3.5 -->
+
+    <!-- jQuery Easing v1.3 -->
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- Library - jQuery.appear -->
+    <script src="libraries/appear/jquery.appear.js"></script>
+
+    <!-- Library - OWL Carousel V.2.0 beta -->
+    <script src="libraries/owl-carousel/owl.carousel.min.js"></script>
+
+    <!-- jQuery For Number Counter -->	
+    <script src="libraries/number/jquery.animateNumber.min.js"></script>
+
+    <!-- Library - Google Map API -->
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+
+    <!-- Library - FlexSlider v2.5.0 -->
+    <script defer src="libraries/flexslider/jquery.flexslider.js"></script>
+
+    <!-- Library - Theme JS -->
+    <script src="js/functions.js"></script>
+    <script src="js/lightbox.js" type="text/javascript"></script>
+    <script>
+        lightbox.option({
+            'resizeDuration': 200,
+            'wrapAround': true
+        })
+    </script>
+</body>
+</html>
