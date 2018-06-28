6085146569585fcad7209a5ccea60ee3adc059ac
mgashini95 <mganisansala@gmail.com>
2018-06-28 16:47:09 +0530

    #15 create gallery page

---
+++ gallery.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Lotus Cabs - Taxi Company HTML5 Template</title>
+
+    <!-- Standard Favicon -->
+    <link rel="icon" type="image/x-icon" href="images/lotus-logo.png" />
+    <!-- For iPhone 4 Retina display: -->
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114x114-precomposed.png">
+    <!-- For iPad: -->
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72x72-precomposed.png">
+    <!-- For iPhone: -->
+    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-57x57-precomposed.png">	
+    <!-- Library - Loader CSS -->
+    <link rel="stylesheet" type="text/css" href="libraries/loader/loaders.min.css">
+
+    <!-- Library - Google Font Familys -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,100italic,100,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Library - Bootstrap v3.3.5 -->
+    <link rel="stylesheet" type="text/css" href="libraries/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="libraries/bootstrap/bootstrap-datetimepicker.min.css">
+    <!-- Font Icons -->
+    <link rel="stylesheet" type="text/css" href="libraries/fonts/font-awesome.min.css">
+    <!-- Library - OWL Carousel V.2.0 beta -->
+    <link rel="stylesheet" type="text/css" href="libraries/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="libraries/owl-carousel/owl.theme.css">
+    <!-- Library - FlexSlider v2.5.0 -->
+    <link rel="stylesheet" type="text/css" href="libraries/flexslider/flexslider.css">
+    <!-- Library - Animate CSS -->
+    <link rel="stylesheet" type="text/css" href="libraries/animate/animate.min.css">
+    <!-- Custom - Common CSS -->
+    <link rel="stylesheet" type="text/css" href="css/plugins.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation-menu.css">
+    <!-- Custom - Theme CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/shortcodes.css">
+    <link href="css/lightbox.css" rel="stylesheet" type="text/css"/>
+    <link href="css/simplelightbox.min.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
+    <!-- Loader -->
+    <div id="site-loader" class="load-complete">
+        <div class="loader">
+            <div class="loader-inner ball-clip-rotate">
+                <div></div>
+            </div>
+        </div>
+    </div><!-- Loader /- -->
+
+    <a id="top"></a>
+    <!-- Main Container -->
+    <div class="main-container">
+
+        <!-- Header -->
+        <?php include './header.php'; ?>
+        <!-- Header /- -->
+
+        <!-- Banner Section -->
+        <div id="page-banner" class="page-banner faq-banner container-fluid no-padding">
+            <div class="page-heading">
+                <h3>Gallery</h3>
+                <ol class="breadcrumb">
+                    <li><a href="#">Home</a></li>
+                    <li class="active"><a href="#">Gallery</a></li>
+                </ol>
+            </div>
+        </div>
+        <!-- Banner Section /- -->
+        <section id="gallery">
+            <div class="container">
+            <div class="col-md-3">
+                <div class="gallery">
+                    <a href="images/services/service-1.jpg" class="big"><img src="images/services/service-1.jpg" alt="" title="lotus-Galle" style="padding-bottom: 31px; padding-top: 40px;"/></a>
+                    <div class="clear"></div>
+                </div>
+            </div>
+            </div>
+        </section>
+        
+
+
+
+        <!--Footer -->
+        <?php include './footer.php'; ?>
+        <!--Footer End-->
+
+
+    </div><!-- Main Container -->
+    <!-- JQuery v1.11.3 -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Library - Modernizer -->
+    <script src="libraries/modernizr/modernizr.js"></script>
+
+    <!-- Library - Bootstrap v3.3.5 -->
+    <script src="libraries/bootstrap/bootstrap.min.js"></script><!-- Bootstrap JS File v3.3.5 -->
+    <script src="libraries/bootstrap/bootstrap-datetimepicker.min.js"></script><!-- Bootstrap JS File v3.3.5 -->
+
+    <!-- jQuery Easing v1.3 -->
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- Library - jQuery.appear -->
+    <script src="libraries/appear/jquery.appear.js"></script>
+
+    <!-- Library - OWL Carousel V.2.0 beta -->
+    <script src="libraries/owl-carousel/owl.carousel.min.js"></script>
+
+    <!-- jQuery For Number Counter -->	
+    <script src="libraries/number/jquery.animateNumber.min.js"></script>
+
+    <!-- Library - Google Map API -->
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+
+    <!-- Library - FlexSlider v2.5.0 -->
+    <script defer src="libraries/flexslider/jquery.flexslider.js"></script>
+
+    <!-- Library - Theme JS -->
+    <script src="js/functions.js"></script>
+    <link href="css/simplelightbox.min.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript">/* <![CDATA[ */(function (d, s, a, i, j, r, l, m, t) {
+            try {
+                l = d.getElementsByTagName('a');
+                t = d.createElement('textarea');
+                for (i = 0; l.length - i; i++) {
+                    try {
+                        a = l[i].href;
+                        s = a.indexOf('/cdn-cgi/l/email-protection');
+                        m = a.length;
+                        if (a && s > -1 && m > 28) {
+                            j = 28 + s;
+                            s = '';
+                            if (j < m) {
+                                r = '0x' + a.substr(j, 2) | 0;
+                                for (j += 2; j < m && a.charAt(j) != 'X'; j += 2)
+                                    s += '%' + ('0' + ('0x' + a.substr(j, 2) ^ r).toString(16)).slice(-2);
+                                j++;
+                                s = decodeURIComponent(s) + a.substr(j, m - j)
+                            }
+                            t.innerHTML = s.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+                            l[i].href = 'mailto:' + t.value
+                        }
+                    } catch (e) {
+                    }
+                }
+            } catch (e) {
+            }
+        })(document);/* ]]> */</script>
+    <script src="js/simple-lightbox.min.js" type="text/javascript"></script>
+    <script src="js/lightbox.js" type="text/javascript"></script>
+    <script>
+        $(function () {
+            var $gallery = $('.gallery a').simpleLightbox();
+
+            $gallery.on('show.simplelightbox', function () {
+                console.log('Requested for showing');
+            })
+                    .on('shown.simplelightbox', function () {
+                        console.log('Shown');
+                    })
+                    .on('close.simplelightbox', function () {
+                        console.log('Requested for closing');
+                    })
+                    .on('closed.simplelightbox', function () {
+                        console.log('Closed');
+                    })
+                    .on('change.simplelightbox', function () {
+                        console.log('Requested for change');
+                    })
+                    .on('next.simplelightbox', function () {
+                        console.log('Requested for next');
+                    })
+                    .on('prev.simplelightbox', function () {
+                        console.log('Requested for prev');
+                    })
+                    .on('nextImageLoaded.simplelightbox', function () {
+                        console.log('Next image loaded');
+                    })
+                    .on('prevImageLoaded.simplelightbox', function () {
+                        console.log('Prev image loaded');
+                    })
+                    .on('changed.simplelightbox', function () {
+                        console.log('Image changed');
+                    })
+                    .on('nextDone.simplelightbox', function () {
+                        console.log('Image changed to next');
+                    })
+                    .on('prevDone.simplelightbox', function () {
+                        console.log('Image changed to prev');
+                    })
+                    .on('error.simplelightbox', function (e) {
+                        console.log('No image found, go to the next/prev');
+                        console.log(e);
+                    });
+        });
+    </script>
+</body>
+
+</html>
